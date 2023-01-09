4d053979caf7d569fc68346da619baa20df3552d
davidlopez <dfl11@alu.ua.es>
2023-01-09 22:12:02 +0100

    Initial commit

---
+++ app/src/main/java/es/ua/eps/practicaficheros/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.ua.eps.practicaficheros
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
